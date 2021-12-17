a682e8cb81faf83c68ea6b5897b5332356dd426d
Davi <davibnovaes@hotmail.com>
2021-12-17 03:54:34 -0300

    filmes servidor: adicionado

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 export default function Main(){
+    const [film, setFilm] = useState([])
+    
+
+    useEffect(()=> {
+        const promisse = axios.get("https://mock-api.driven.com.br/api/v4/cineflex/movies")
+
+        promisse.then((resp => {
+            setFilm(resp.data)
+            
+        }))
+    },[])
+
+    
+
     return(
         <div className="filmsContent">
             <div className="spanTop">
                 <span >Selecione o filme</span>
             </div>
             <div className="films">
-                <div className="film">
-                     <img src="./img/image3.png" alt="2067" />
-                </div>
-                <div className="film">
-                    <img src="./img/image6.png" alt="enola" />
-                </div>
-                <div className="film">
-                    <img src="./img/image6.png" alt="enola" />
-                </div>
-                <div className="film">
-                    <img src="./img/image6.png" alt="enola" />
-                </div>
-                <div className="film">
-                    <img src="./img/image6.png" alt="enola" />
-                </div>
+                {film.map((posterURL, index)=> 
+                    <div className="film">
+                        <img src={film[index].posterURL}  alt={film[index].title}/>
+                    </div>
+                )}
+                
+                
             </div>
         </div>
     )
